a54cff1e070a6a828a8888ba86a1ee4566fbeab0
daveyo89 <j.tothdavid@gmail.com>
2022-07-12 21:30:23 +0200

    can find nouns but not very nice

--- translate.py
+++ translate.py
@@ -2,7 +2,7 @@ import urllib.parse
 import pandas as pd
 from tqdm import tqdm
 
-target_lang = 'de'
+target_lang = 'hu'
 wiki_page = f'https://sv.wiktionary.org/wiki/'
 
 
@@ -12,22 +12,36 @@ def translate():
     excel_df = pd.read_excel('data.xlsx', index_col=0)
 
     for index, row in tqdm(excel_df.iterrows(), total=len(excel_df)):
+        index_size = len(index.split())
         j += 1
         try:
-            df = pd.read_html(wiki_page + urllib.parse.quote(index))
-            i = 0
-            word_dict = dict(zip(df[i].iloc[:, 0], df[i].iloc[:, 1]))
-            while not word_dict.get('Infinitiv'):
-                i += 1
+            if index_size <= 1:
+                df = pd.read_html(wiki_page + urllib.parse.quote(index))
+                i = 0
                 word_dict = dict(zip(df[i].iloc[:, 0], df[i].iloc[:, 1]))
-            excel_df.loc[word_dict['Infinitiv']] = [word_dict.get('Presens'), word_dict.get('Preteritum'),
-                                                    word_dict.get('Supinum'), word_dict.get('Imperativ'),
-                                                    f'=GOOGLETRANSLATE(A{j} ; "sv"; "en")',
-                                                    f'=GOOGLETRANSLATE(A{j} ; "sv"; "{target_lang}")']
-        except Exception as e:
-            print(word_dict, e)
-            pass
+                while not word_dict.get('Infinitiv'):
+                    i += 1
+                    word_dict = dict(zip(df[i].iloc[:, 0], df[i].iloc[:, 1]))
 
+                excel_df.loc[word_dict['Infinitiv']] = [word_dict.get('Presens'), word_dict.get('Preteritum'),
+                                                        word_dict.get('Supinum'), word_dict.get('Imperativ'),
+                                                        f'=GOOGLETRANSLATE(A{j} ; "sv"; "en")',
+                                                        f'=GOOGLETRANSLATE(A{j} ; "sv"; "{target_lang}")']
+            else:
+                df = pd.read_html(wiki_page + urllib.parse.quote(index.split()[-1]))
+                i = 0
+                while not 'Singular' in df[i].columns:
+                    i += 1
+
+                word_dict = dict(zip(['Singular', 'Plural'], [df[i]['Singular'].iloc[:,0][0], df[i]['Plural'].iloc[:,0][0]]))
+                excel_df.loc[index] = [word_dict.get('Singular'), word_dict.get('Plural'), "-", "-", f'=GOOGLETRANSLATE(A{j} ; "sv"; "en")',
+                                       f'=GOOGLETRANSLATE(A{j} ; "sv"; "{target_lang}")']
+
+        except Exception as e:
+            excel_df.loc[index] = ["-", "-", "-", "-", f'=GOOGLETRANSLATE(A{j} ; "sv"; "en")', f'=GOOGLETRANSLATE(A{j} ; "sv"; "{target_lang}")']
+            print(index, row, word_dict, e)
+            pass
+    print(excel_df)
     writer = pd.ExcelWriter('done.xlsx', engine='xlsxwriter')
     excel_df.to_excel(writer, sheet_name='translations')
 
